9c206f5238a56d886a5ef61728e0bf99df2ab3b1
アレチ <angelisium@daum.net>
2022-08-24 23:40:52 +0200

    Update main.ts

--- source/main.ts
+++ source/main.ts
@@ -1,17 +1,35 @@
+import 'dotenv/config';
 import express from 'express';
 import nunjucks from 'nunjucks';
+import { MongoClient } from 'mongodb';
 import { route as HomeController } from './route/home';
 
-const port = process.argv?.[2] || 3000;
-const app = express();
-nunjucks.configure('source/view', {
-	autoescape: true,
-	express: app
-});
+(async function (port: number) {
+	if (!process.env.MONGO_URL) {
+		return console.error("unknown mongo url");
+	}
+	const exprs = express();
+	const mongo = new MongoClient(process.env.MONGO_URL);
+	try {
+		await mongo.connect();
+		console.log("Connected correctly to server");
+	} catch (e) {
+		if(typeof e === "string") {
+			return console.error(e);
+		} else if(e instanceof Error) {
+			return console.error(e.message);
+		}
+	}
 
-app.get('/', HomeController);
-app.use(express.static('public'));
+	nunjucks.configure('source/view', {
+		autoescape: true,
+		express: exprs
+	});
 
-app.listen(port, () => {
-	console.log('listen 127.0.0.1:' + port);
-});
+	exprs.get('/', HomeController);
+	exprs.use(express.static('public'));
+
+	exprs.listen(port, () => {
+		console.log('listen 127.0.0.1:' + port);
+	});
+})(parseInt(process.argv?.[2] || '3000'));
