0be8fdb88a8d8cf168a18d03acb7de4778083c4b
アレチ <angelisium@daum.net>
2022-08-24 22:42:27 +0200

    Update main.ts

--- source/main.ts
+++ source/main.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import nunjucks from 'nunjucks';
 import { route as HomeController } from './route/home';
 
+const port = process.argv?.[2] || 3000;
 const app = express();
 nunjucks.configure('source/view', {
 	autoescape: true,
@@ -11,8 +12,6 @@ nunjucks.configure('source/view', {
 app.get('/', HomeController);
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-	console.log('The application is listening on port 3000!');
+app.listen(port, () => {
+	console.log('listen 127.0.0.1:' + port);
 });
-
-// soon add https + 'yarn prod <port>' or 'yarn start <port>'
