f97cc6c485d980a1b0704e2e6f798d57d3881f4f
アレチ <angelisium@daum.net>
2022-08-25 17:27:06 +0200

    Update Home.ts

--- source/route/Home.ts
+++ source/route/Home.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
 
 export async function route(req: Request, res: Response) {
-	let locale = "en",
+	let locale = req.params?.locale || "en",
 		games = await mongo.collection("game").find({ locale }).toArray();
-	res.render('home.njk', { locale, games });
+	res.render('home.njk', { locale, games,
+		user: req.query?.user ? {
+			username: "Angelisium",
+			avatar: "//imgup.motion-twin.com//twinoid/2/7/38ff77a6_696209.jpg"
+		} : false
+	});
 }
