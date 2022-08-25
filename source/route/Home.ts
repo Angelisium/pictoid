bf82f38ead73e07adc13addc6a17ebfeb2594e09
アレチ <angelisium@daum.net>
2022-08-25 18:35:20 +0200

    Update Home.ts

--- source/route/Home.ts
+++ source/route/Home.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export async function route(req: Request, res: Response) {
-	let locale = req.params?.locale || "en",
+	let locale = req.params?.locale || "all",
 		games = await mongo.collection("game").find({ locale }).toArray();
 	res.render('home.njk', { locale, games,
 		user: req.query?.user ? {
