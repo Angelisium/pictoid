1aa7e8372f90d33be05bb45974289765758fbb38
Thomas Lupin <mail+git@thetoto.fr>
2022-08-25 19:51:38 +0200

    add some dotfiles

--- source/route/Home.ts
+++ source/route/Home.ts
@@ -3,7 +3,8 @@ import { Request, Response } from 'express';
 export async function route(req: Request, res: Response) {
 	let locale = req.params?.locale || "all",
 		games = await mongo.collection("game").find({ locale }).toArray();
-	res.render('home.njk', { locale, games,
+	res.render('home.njk', {
+		locale, games,
 		user: req.query?.user ? {
 			username: "Angelisium",
 			avatar: "//imgup.motion-twin.com//twinoid/2/7/38ff77a6_696209.jpg"
